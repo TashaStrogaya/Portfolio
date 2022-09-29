1031ac28476bdde7bbfc33cf5bb5092686612401
Natasha Safronova <94457858+TashaStrogaya@users.noreply.github.com>
2022-09-29 10:42:43 +0300

    And alerts

---
+++ telegram_bot_with_airflow/send_alerts.py
@@ -0,0 +1,170 @@
+#подключаемся к библиотекам
+import pandas as pd
+import pandahouse as ph
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from datetime import datetime, timedelta
+from io import StringIO
+import requests
+from airflow.decorators import dag, task
+from airflow.operators.python import get_current_context
+import numpy as np
+
+import telegram
+import io
+
+# функция для извлечения данных
+def ch_get_df(query='Select 1', host='https://clickhouse.lab.karpov.courses', db = 'simulator_20220820', user='student', password='dpo_python_2020'):
+    
+    connection = {'host': host,
+                      'database': db,
+                      'user': user, 
+                      'password': password
+                     }
+
+    df = ph.read_clickhouse(query, connection=connection)
+    return df
+
+# Дефолтные параметры, которые прокидываются в таски
+default_args = {
+    'owner': 'n.safronova',
+    'depends_on_past': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2),
+    'start_date': datetime(2022, 9, 11, 0, 0, 0) #,
+}
+
+# Интервал запуска DAG
+schedule_interval = '*/15 * * * *'
+
+
+@dag(default_args=default_args, schedule_interval=schedule_interval, catchup=False)
+def dag_catch_your_alerts():
+
+    
+    @task()
+    def extract():
+        # Забираем данные
+        query = '''
+        select 
+            feed.time as time,
+            time_event,
+            dau_feed,
+            likes,
+            views,
+            ctr,
+            dau_messenger,
+            messeges
+        from (
+            select
+                toStartOfFifteenMinutes(time) as time,
+                formatDateTime(time, '%m/%d %R') as time_event,        
+                uniqExact(user_id) as dau_feed,
+                countIf(action, action = 'like') as likes,
+                countIf(action, action = 'view') as views,
+                round(likes/views * 100 , 2) as ctr
+            from
+                simulator_20220820.feed_actions  
+            WHERE 
+                time >= today() - 1 
+                and time < toStartOfFifteenMinutes(now())
+            group by 
+                toStartOfFifteenMinutes(time) as time
+        ) feed
+        
+        join (
+            select
+                toStartOfFifteenMinutes(time) as time,    
+                uniqExact(user_id) as dau_messenger,
+                count() as messeges
+            from
+                simulator_20220820.message_actions  
+            WHERE 
+                time >= today() - 1 
+                and time < toStartOfFifteenMinutes(now())
+            group by 
+                toStartOfFifteenMinutes(time) as time
+        ) msg
+        using time
+        order by time
+            '''
+
+        df = ch_get_df(query)
+        return df
+    
+    my_token = '5669503540:AAGDvBAeH7fJf0-7uJM260hQY_zGEr5lBZ4'
+    # my_token = os.environ.get("REPORT_BOT_TOKEN")
+    bot = telegram.Bot(token=my_token)
+    
+    # функция проверки метрики на алерт
+    def check_metric(df, metric, n = 5, a = 3):
+    
+        df['q75'] = df[metric].shift(1).rolling(n, min_periods=1).quantile(0.75)
+        df['q25'] = df[metric].shift(1).rolling(n, min_periods=1).quantile(0.25)
+        
+        df['iqr'] = df['q75'] - df['q25']
+        df['up'] = df['q75'] + a * df['iqr']
+        df['low'] = df['q25'] - a * df['iqr']
+        
+        df['up'] = df.up.rolling(3, min_periods = 1).mean()
+        df['low'] = df.low.rolling(3, min_periods = 1).mean()
+        
+        if df[metric].iloc[-1] < df['low'].iloc[-1] or  df[metric].iloc[-1] > df['up'].iloc[-1]:
+            return df, 1
+        return df, 0
+    
+    # Функция отправки алертов
+    @task
+    def send_alert(df, chat_id = 770699869):
+
+        metrics = ['dau_feed', 'likes', 'views', 'ctr','dau_messenger', 'messeges']
+        
+        # Проверяем в цикле нормальность метрик
+        for metric in metrics:
+            df, flag_alert = check_metric(df, metric)
+            
+            # Если метрика дала алерт, формируем и отправляем сообщение
+            if flag_alert == 1:
+                
+                messege = f"Коллега, алёрт по метрике {metric}\n" + \
+                        f"Текущее значение: {df[metric].iloc[-1]}. Отклонение от предыдущего: {(int(df[metric].iloc[-1]) - int(df[metric].iloc[-2]))/df[metric].iloc[-2]*100:.2f}%\n" + \
+                        f"Расчёт ведётся с окном в 15 минут.\n" + \
+                        f"Ссылка на дашборд: https://superset.lab.karpov.courses/superset/dashboard/1784/"
+                bot.sendMessage(chat_id=chat_id, text=messege)
+                print(messege)
+                
+                sns.set(rc={'figure.figsize':(16, 8)})
+                
+                ax = sns.lineplot(data = df, x = 'time_event', y = 'up', color = '#228b22')
+                ax = sns.lineplot(data = df, x = 'time_event', y = 'low', color = '#228b22')
+                ax = sns.lineplot(data = df, x = 'time_event', y = metric, color = '#1c1c1c', label = metric.upper())
+                
+                ax.fill_between(x = df.time_event, y1 = df.low, y2 = df.up, color = '#228b22', alpha = 0.2, label = 'Зона нормальности')
+
+                sticks_x = []
+                for ind, item in enumerate(df.time_event.unique()):
+                                if ind % 12 == 0:
+                                    sticks_x.append(item)            
+                ax.set_xticks(sticks_x)
+                
+                plt.title(f'Алёрт по метрике {metric.upper()}', size = 16)
+                plt.legend(loc = 'lower right')
+                plt.ylabel('')
+                plt.xlabel('')
+
+                plt.tight_layout()
+                plot_object = io.BytesIO()
+                plt.savefig(plot_object)
+                plot_object.seek(0)
+                plot_object.name = 'alert.png'
+                plt.close()
+
+                bot.sendPhoto(chat_id=chat_id, photo=plot_object)
+                print(f'ALERT with {metric}! Plots sent successfuly.')
+    
+    # Запуск таска
+    send_alert(extract(), -721111166)
+    
+# Объявление дага
+dag_catch_your_alerts = dag_catch_your_alerts()
